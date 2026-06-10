98bc03601b28e3f345ad316227312d2b83ebbe19
dentech-softwares <dennisnjenga2060@gmail.com>
2026-06-10 16:59:35 +0300

    web icon

--- includes/admin_header.php
+++ includes/admin_header.php
@@ -43,7 +43,7 @@ function getInitials($name) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard - Murang'a County Dairy</title>
-    <link rel="icon" type="image/png" href="../muranga.png">
+    <link rel="icon" type="image/svg+xml" href="../favicon.svg">
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/responsive.css">
     <link rel="stylesheet" href="../assets/css/all.min.css">
