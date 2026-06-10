bf22b100f6877eeec631793b3fe925828bdb8f87
dentech-softwares <dennisnjenga2060@gmail.com>
2026-06-10 18:38:40 +0300

    changes

--- includes/attendant_header.php
+++ includes/attendant_header.php
@@ -46,7 +46,7 @@ function getInitials($name) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Attendant Dashboard - <?php echo $dairy_name; ?></title>
-    <link rel="icon" type="image/svg+xml" href="../favicon.svg">
+    <link rel="icon" type="image/png" href="../muranga.png">
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/responsive.css">
     <link rel="stylesheet" href="../assets/css/all.min.css">
